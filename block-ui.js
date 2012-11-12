8b718a6aaab5a56e065b7aa6c69251503021dd06
Gpx <polvara@gmail.com>
2012-11-12 20:40:32 +0100

    Added message element

--- block-ui.js
+++ block-ui.js
@@ -2,10 +2,11 @@
   
   window.blockUI = {}; // Library namespace
 
-  var openBlocks = 0, // Counter fon open blocks
-      blockDiv = document.createElement('div'); // Blocking div
+  var openBlocks = 0,                                  // Counter fon open blocks
+      blockDiv = document.createElement('div'),        // Blocking div
+      messageElement = document.createElement('span'); // HTML element that displays a loading message
   
-  // Set blocking div style and append it to the <body>
+  // Set blocking div style
   blockDiv.style.height          = '100%';
   blockDiv.style.width           = '100%';
   blockDiv.style.position        = 'absolute';
@@ -13,7 +14,16 @@
   blockDiv.style.left            = '0';
   blockDiv.style.backgroundColor = 'rgba(255, 0, 0, .5)';
   blockDiv.style.display         = 'none';
-
+  blockDiv.style.verticalAlign   = 'middle';
+  blockDiv.style.textAlign       = 'right';
+  
+  // Set message element style
+  messageElement.innerText = 'Loading...';
+  messageElement.style.padding = '2px 15px';
+  messageElement.style.backgroundColor = 'rgba(255, 0, 0, .5)';
+  messageElement.style.borderBottomLeftRadius = '5px';
+  
+  blockDiv.appendChild(messageElement);
   document.body.appendChild(blockDiv);
 
   var showBlockDiv = function () {
@@ -29,7 +39,9 @@
   // Public methods
 
   window.blockUI.setBackground = function ( r, g, b, a ) {
+    /** Set the background color */
     blockDiv.style.backgroundColor = 'rgba('+r+', '+g+', '+b+', '+a+')';
+    messageElement.style.backgroundColor = 'rgba('+r+', '+g+', '+b+', 1)';
   };
 
   window.blockUI.block = function () {
